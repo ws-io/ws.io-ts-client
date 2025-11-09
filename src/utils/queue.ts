9c9125d141c9e920a3bef7e13e86d6fd6a230ce4
kiki-kanri <a470666@gmail.com>
2025-11-09 14:12:23 +0800

    refactor: update `AsyncQueue`
    - use `yocto-queue` for internal queue and resolvers
    - add `asyncIterator`
    - rename `close` to `closeAndClear`

--- src/utils/queue.ts
+++ src/utils/queue.ts
@@ -1,10 +1,20 @@
 import type { Promisable } from 'type-fest';
+import Queue from 'yocto-queue';
 
 export class AsyncQueue<T> {
     // Private properties
-    #closed: boolean = false;
-    #queue: T[] = [];
-    #resolvers: ((value: Promisable<null | T>) => void)[] = [];
+    #closed = false;
+    #queue = new Queue<T>();
+    #resolvers = new Queue<((value: Promisable<null | T>) => void)>();
+
+    // Symbols
+    async* [Symbol.asyncIterator]() {
+        while (true) {
+            const value = await this.recv();
+            if (value === null) break;
+            yield value;
+        }
+    }
 
     // Public getters
     get closed() {
@@ -12,32 +22,32 @@ export class AsyncQueue<T> {
     }
 
     // Public methods
-    close() {
+    closeAndClear() {
         this.#closed = true;
-        this.#resolvers.forEach((resolver) => resolver(null));
-        this.#resolvers.length = 0;
+        this.#queue.clear();
+        for (const resolver of this.#resolvers.drain()) resolver(null);
     }
 
-    recv() {
-        if (this.#queue.length) return this.#queue.shift();
+    recv(): Promisable<null | T> {
+        if (this.#queue.size) return this.#queue.dequeue()!;
         if (this.#closed) return null;
-        return new Promise<null | T>((resolve) => void this.#resolvers.push(resolve));
+        return new Promise<null | T>((resolve) => void this.#resolvers.enqueue(resolve));
     }
 
     reopen() {
         this.#closed = false;
     }
 
-    send(item: T) {
+    send(value: T) {
         if (this.#closed) throw new Error('Queue is closed');
-        const resolver = this.#resolvers.shift();
-        if (resolver) resolver(item);
-        else this.#queue.push(item);
+        const resolver = this.#resolvers.dequeue();
+        if (resolver) resolver(value);
+        else this.#queue.enqueue(value);
     }
 
-    trySend(item: T) {
+    trySend(value: T) {
         try {
-            this.send(item);
+            this.send(value);
             return true;
         } catch {
             return false;
